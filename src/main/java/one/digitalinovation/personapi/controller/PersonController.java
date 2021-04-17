fd922f4e1bde9c6e2cabe1bb3ce88a6b69ccd2bb
Marcus Davi Argolo Ribeiro <marcusdavi@gmail.com>
2021-04-17 19:50:08 -0300

    delete and update person

--- src/main/java/one/digitalinovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinovation/personapi/controller/PersonController.java
@@ -6,9 +6,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -47,6 +49,17 @@ public class PersonController {
 	public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
 		return personService.findById(id);
 
+	}
+	
+	@PutMapping("/{id}")
+	public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
+		return personService.updateById(id, personDTO);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+		personService.deleteById(id);
 	}
 
 }
--- src/main/java/one/digitalinovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinovation/personapi/service/PersonService.java
@@ -1,9 +1,9 @@
 package one.digitalinovation.personapi.service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,9 +16,9 @@ import one.digitalinovation.personapi.repository.PersonRepository;
 
 @Service
 public class PersonService {
-	
+
 	private PersonRepository personRepository;
-	
+
 	private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
 	@Autowired
@@ -27,32 +27,53 @@ public class PersonService {
 	}
 
 	public MessageResponseDTO createPerson(PersonDTO personDTO) {
-		
+
 		Person personToSave = personMapper.toModel(personDTO);
-		
+
 		Person personCreated = personRepository.save(personToSave);
-		return MessageResponseDTO
-				.builder()
-				.message("Created person with ID " + personCreated.getId())
-				.build();
-		
+		return createMessageResponse(personCreated.getId(), "Created");
+
 	}
 
 	public List<PersonDTO> listAll() {
 		List<Person> allPeople = personRepository.findAll();
-		
-		return allPeople.stream()
-				.map(personMapper::toDTO)
-				.collect(Collectors.toList());
-		
+
+		return allPeople.stream().map(personMapper::toDTO).collect(Collectors.toList());
+
 	}
 
 	public PersonDTO findById(Long id) throws PersonNotFoundException {
-		Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
-		
+		Person person = verifyIfExists(id);
+
 		return personMapper.toDTO(person);
 
+	}
 
+	public void deleteById(Long id) throws PersonNotFoundException {
+		verifyIfExists(id);
+
+		personRepository.deleteById(id);
+
+	}
+
+	public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+		 verifyIfExists(id);
+
+		Person personToUpdate = personMapper.toModel(personDTO);
+		personToUpdate.setId(id);
+
+		personRepository.save(personToUpdate);
+		
+		return createMessageResponse(id, "Updated");
+
+	}
+
+	private Person verifyIfExists(Long id) throws PersonNotFoundException {
+		return personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+	}
+
+	private MessageResponseDTO createMessageResponse(Long id, String operation) {
+		return MessageResponseDTO.builder().message(String.format("%s person with ID %d", operation, id)).build();
 	}
 
 }
